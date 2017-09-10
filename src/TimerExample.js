078879ff6d4613b88b189106a9e2064fc9bf677f
HyperSimon <hero21c@outlook.com>
2017-09-10 16:41:15 +0800

    新增 VisibleView的使用

--- src/TimerExample.js
+++ src/TimerExample.js
@@ -2,26 +2,38 @@ import React, {Component} from 'react';
 
 import TimerUI from "./TimerUI";
 import Timer from "rn-timer";
+import VisibleView from "rn-visible-view";
+import {Text} from "react-native";
 
 
 export default class TimerExample extends Component {
+
+  state: {
+    visible: false
+  }
+
   render() {
-    const binder = {
+    const visibleBinder = {
+      visible: (date: Date) => date.getSeconds() % 2 === 1,
+    };
+    const timerBinder = {
       hour: (date: Date) => date.getUTCHours(),
       min: (date: Date) => date.getMinutes(),
       second: (date: Date) => date.getSeconds(),
-    };
+    }
 
     return (
       <Timer
+        style={{marginTop: 20}}
         timerStep={10}
-        binders={binder}
-        timestamp={new Date().getTime() + 50000}
-        timerFinishedListener={() => {
-          alert('end')
-        }}>
+        binders={visibleBinder}
+        timestamp={new Date().getTime() + 50000}>
 
-        <TimerUI/>
+        <VisibleView>
+          <Text>
+            this is demo
+          </Text>
+        </VisibleView>
 
       </Timer>
     );
